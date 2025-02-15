8a96c8f70eac2200ad63368480a65765631060e6
ghafari <z.ghaffaripour@gmail.com>
2025-02-15 09:35:02 -0800

    navbar and basepage added

---
+++ full_web_app_with_reflex/full_web_app_with_reflex/ui/base.py
@@ -0,0 +1,22 @@
+import reflex as rx
+from .nav import navbar
+
+
+
+def base_page(child: rx.Component, hide_navbar= False, *args, **kwargs )->rx.Component:
+    # print([type(x) for x in args])
+    if not isinstance(child, rx.Component):
+        child = rx.heading("it is not child")
+    if hide_navbar:
+        return rx.container(
+            child,
+            rx.logo(),
+            rx.color_mode.button(position="bottom-right"), 
+        )
+    return rx.container(
+        navbar(),        
+        child,
+        rx.logo(),
+        rx.color_mode.button(position="bottom-right"), 
+             
+    )
